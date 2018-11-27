14839e919b7a0b11e8094e454545e21b18dd993b
Valkaryne <anix.mig29@gmail.com>
2018-11-27 21:52:10 +0300

    Sign Out feature implemented

--- loginui/src/main/java/com/cybernatica/ngale/loginui/SecondActivity.java
+++ loginui/src/main/java/com/cybernatica/ngale/loginui/SecondActivity.java
@@ -1,15 +1,21 @@
 package com.cybernatica.ngale.loginui;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class SecondActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    static final String TAG = "SecTag";
 
     private TextView tvProfileEmail;
 
@@ -22,9 +28,7 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        navigationView = findViewById(R.id.navigationView);
-
-        tvProfileEmail = navigationView.getHeaderView(0).findViewById(R.id.tvProfileEmail);
+        initNavigationView();
 
         firebaseAuth = FirebaseAuth.getInstance();
 
@@ -34,9 +38,27 @@ public class SecondActivity extends AppCompatActivity {
         tvProfileEmail.setText("User: " + email);
     }
 
+    private void initNavigationView() {
+        navigationView = findViewById(R.id.navigationView);
+        navigationView.bringToFront();
+        tvProfileEmail = navigationView.getHeaderView(0).findViewById(R.id.tvProfileEmail);
+
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.item_logout:
+                signOut();
+                break;
+        }
+        return true;
+    }
+
+    private void signOut() {
         firebaseAuth.signOut();
+        finish();
+        startActivity(new Intent(this, MainActivity.class));
     }
 }
